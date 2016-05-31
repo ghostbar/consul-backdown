779d57ca3bc77456fd609126b8593c68d6502ded
Jose-Luis Rivas <me@ghostbar.co>
2016-05-31 10:55:03 -0500

    build script

---
+++ scripts/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+# vim: ft=sh tw=80
+
+BIN_NAME="consul-backdown"
+TARGETS="linux;amd64 linux;arm darwin;amd64"
+
+if [ -d build ]; then
+  rm build/*
+else
+  mkdir build
+fi
+
+for target in $TARGETS; do
+  targetos=$(echo "$target" | awk -F ";" '{ print $1 }')
+  targetarch=$(echo "$target" | awk -F ";" '{ print $2 }')
+  env GOOS="$targetos" GOARCH="$targetarch" go build
+  mv $BIN_NAME build/"$BIN_NAME"_"$targetos"_"$targetarch"
+done
+
+cd build && shasum -a 512 "$BIN_NAME"_* > SHA512SUMS.txt && cd .. || exit
+${GPG} --clearsign build/SHA512SUMS.txt
