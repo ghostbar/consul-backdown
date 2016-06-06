601f6cb4a98d0f196087c821410ecb28d3b3bc5f
Jose-Luis Rivas <me@ghostbar.co>
2016-06-06 14:45:19 -0500

    restore and separator options implemented

--- consul-backdown.go
+++ consul-backdown.go
@@ -4,10 +4,12 @@ import (
 	"github.com/docopt/docopt-go"
 	"github.com/hashicorp/consul/api"
 
+	"bufio"
 	"encoding/base64"
 	"fmt"
 	"os"
 	"sort"
+	"strings"
 )
 
 const version = "consul-backdown 1.0.0"
@@ -15,18 +17,21 @@ const usage = `consul-backdown.
 
 Usage:
   consul-backdown backup [--url <url> | -u <url>] [-t <token> | --token <token>]
-				  [ -d | --debug ]
+				  [ -d | --debug ] [ -s <sep> | --separator <sep> ]
   consul-backdown restore [-u <url> | --url <url>] [-t <token> | --token <token>]
-				  [ -d | --debug ]
+				  [ -d | --debug ] [ -i <file> | --input <file ]
+				  [ -s <sep> | --separator <separator> ]
   consul-backdown -h | --help
   consul-backdown --version
 
 Options:
-  -u <url> --url=<url>           Consul's HTTP address [default: 127.0.0.1:8500].
-  -t <token> --token=<token>     Consul's token [default: ].
-  -d --debug                     Show more values in the output (don't use it on production) [default: false].
-  -h --help                      Show this screen.
-  --version                      Show version.
+  -u <url> --url=<url>         Consul's HTTP address [default: 127.0.0.1:8500].
+  -t <token> --token=<token>   Consul's token [default: ].
+  -i <file> --input=<file>     Input file for restore, defaults to stdin.
+  -s <sep> --separator=<sep>   Key/Value separator [default: |||].
+  -d --debug                   Show more values in the output (don't use it on production) [default: false].
+  -h --help                    Show this screen.
+  --version                    Show version.
 `
 
 type KVPairs api.KVPairs
@@ -43,7 +48,7 @@ func (a KVPairs) Less(i, j int) bool {
 	return a[i].CreateIndex < a[j].CreateIndex
 }
 
-func backup(ip, token string) {
+func backup(ip, token, separator string) {
 	config := api.DefaultConfig()
 	config.Address = ip
 	config.Token = token
@@ -60,7 +65,40 @@ func backup(ip, token string) {
 
 	for _, element := range pairs {
 		encoded_value := base64.StdEncoding.EncodeToString(element.Value)
-		os.Stdout.WriteString(element.Key + ":" + encoded_value + "\n")
+		os.Stdout.WriteString(element.Key + separator + encoded_value + "\n")
+	}
+}
+
+func restore(ip string, token string, separator string, input *os.File) {
+	config := api.DefaultConfig()
+	config.Address = ip
+	config.Token = token
+
+	client, _ := api.NewClient(config)
+	kv := client.KV()
+
+	scanner := bufio.NewScanner(input)
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	for scanner.Scan() {
+		element := scanner.Text()
+		kvp := strings.Split(element, separator)
+
+		if len(kvp) > 1 {
+			decoded_value, decode_err := base64.StdEncoding.DecodeString(kvp[1])
+			if decode_err != nil {
+				panic(decode_err)
+			}
+
+			p := &api.KVPair{Key: kvp[0], Value: decoded_value}
+			_, err := kv.Put(p, nil)
+			if err != nil {
+				panic(err)
+			}
+		}
 	}
 }
 
@@ -69,17 +107,45 @@ func main() {
 	debug := false
 	url, _ := args["--url"].(string)
 	token, _ := args["--token"].(string)
+	separator, _ := args["--separator"].(string)
 
 	if args["--debug"] == true {
 		debug = true
 		fmt.Println("Arguments being passed:", args)
 	}
+
 	if args["backup"] == true {
 		if debug {
 			fmt.Println("Backing up from server:", url)
 		}
-		backup(url, token)
+		backup(url, token, separator)
 	}
+
 	if args["restore"] == true {
+		if debug {
+			fmt.Println("Restoring into server:", url)
+		}
+
+		if args["--input"] != nil {
+			inputArg, _ := args["--input"].(string)
+
+			if debug {
+				fmt.Println("Will read backup from file given as argument", inputArg)
+			}
+
+			input, err := os.Open(inputArg)
+			if err != nil {
+				panic(err)
+			}
+			defer input.Close()
+			restore(url, token, separator, input)
+		} else {
+			if debug {
+				fmt.Println("Will read backup from STDIN")
+			}
+
+			input := os.Stdin
+			restore(url, token, separator, input)
+		}
 	}
 }
